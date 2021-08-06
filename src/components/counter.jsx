50ac867f631d637517b56c41d87d76256d06a654
David Chavez <davidsaso1234@gmail.com>
2021-08-06 18:51:34 -0500

    adding multiple counter components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,38 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tag1', 'tag2', 'tage3']
     };
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    // handleIncrement() {
+    handleIncrement = product => {
+        console.log(product)
+        this.setState({ count: this.state.count + 1})
+    }
     render() {
         return (
-            <React.Fragment>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
-            </React.Fragment>
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                    onClick={() => this.handleIncrement(this.state)}
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
+            </div>
         );
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
     formatCount() {
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
